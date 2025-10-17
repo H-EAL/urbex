10d76c742271f2ffdf51f5ef1ce92084e3ecb536
Houssem <houssem@3dverse.com>
2025-10-17 13:18:19 -0400

    FPS camera controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 //------------------------------------------------------------------------------
-import { useContext } from "react";
-import { CameraControllerPresets } from "@3dverse/livelink";
+import { useContext, useEffect, useState } from "react";
+import { type Livelink as LivelinkInstance, Entity } from "@3dverse/livelink";
 
 import {
     Livelink,
     Canvas,
     Viewport,
-    CameraController,
-    useCameraEntity,
     LivelinkContext,
+    ViewportContext,
 } from "@3dverse/livelink-react";
 import { LoadingOverlay } from "@3dverse/livelink-react-ui";
 
@@ -18,6 +17,7 @@ import "./App.css";
 //------------------------------------------------------------------------------
 const scene_id = "e58e152c-fa3d-43c5-adbc-8f8d5c77da01";
 const token = "public_J9TC5mDhoFG3YeYB";
+const characterControllerSceneUUID = "7392872d-6fb6-43a2-a8a2-8dc8f1afd014";
 
 //------------------------------------------------------------------------------
 export function App() {
@@ -29,46 +29,79 @@ export function App() {
 }
 
 //------------------------------------------------------------------------------
-const cameraSettings = {
-    voxels: false,
-    // Fog
-    fog: true,
-    fogZeroHeight: 0,
-    fogHeightDensity: 1,
-    fogDistanceDensity: 0.0001,
-    // Env
-    skybox: true,
-    gradient: false,
-    brightness: 0.01,
-    ambientIntensity: 0.01,
-    //Editing
-    grid: false,
-    selectionOutlines: false,
-    // PostFX
-    sharpen: true,
-    filterSpecular: true,
-    bloom: true,
-    bloomThreshold: 1,
-    bloomStrength: 2,
-};
-
-//------------------------------------------------------------------------------
 function AppLayout() {
-    const { cameraEntity } = useCameraEntity({ settings: cameraSettings });
-    const { isConnecting } = useContext(LivelinkContext);
+    const { instance } = useContext(LivelinkContext);
+
+    useEffect(() => {
+        if (!instance) {
+            return;
+        }
+
+        async function instantiatePlayerSceneAndFindThirdPersonCamera(instance: LivelinkInstance) {
+            const playerSceneEntity = await instance.scene.newEntity({
+                name: "PlayerSceneEntity",
+                components: {
+                    local_transform: { position: [0, 1.7, 0] },
+                    scene_ref: { value: characterControllerSceneUUID },
+                },
+                options: {
+                    delete_on_client_disconnection: true,
+                },
+            });
+
+            const children = await playerSceneEntity.getChildren();
+            const thirdPersonCameraEntity = children.find((child) => child.camera !== undefined);
+
+            console.log("Assigning client to scripts");
+            if (thirdPersonCameraEntity && instance.session.client_id) {
+                thirdPersonCameraEntity.assignClientToScripts({
+                    client_uuid: instance.session.client_id,
+                });
+            }
+
+            setStartSimulation(true);
+
+            setCameraEntity(thirdPersonCameraEntity ?? null);
+        }
+        instantiatePlayerSceneAndFindThirdPersonCamera(instance);
+    }, [instance]);
+
+    const [startSimulation, setStartSimulation] = useState<boolean>(false);
+    const [cameraEntity, setCameraEntity] = useState<Entity | null>(null);
 
     return (
-        <Canvas className="h-dvh">
+        <Canvas className="w-full h-full">
             <Viewport cameraEntity={cameraEntity} className="w-full h-full">
-                {!isConnecting && (
-                    <div className="absolute bottom-3 left-1/2 -translate-x-1/2 bg-green-800 text-sm text-white rounded-full py-1 px-3 pointer-events-none animate-[fade-out_.5s_2s_forwards]">
-                        Connected.
-                    </div>
-                )}
-                <CameraController preset={CameraControllerPresets.fly} />
+                {startSimulation && <SimulationStarter />}
             </Viewport>
         </Canvas>
     );
+}
+
+//------------------------------------------------------------------------------
+function SimulationStarter() {
+    const { instance } = useContext(LivelinkContext);
+    const { viewport, viewportDomElement } = useContext(ViewportContext);
+
+    useEffect(() => {
+        if (!instance || !viewport || !viewportDomElement) {
+            return;
+        }
+
+        console.log("Setting up controller");
+
+        if (viewportDomElement.requestPointerLock) {
+            viewportDomElement.requestPointerLock();
+        }
+
+        instance.devices.keyboard.enable();
+        instance.devices.gamepads_registry.enable();
+        instance.devices.mouse.enableOnViewport({ viewport });
+
+        instance.startSimulation();
+    }, [instance, viewport, viewportDomElement]);
+
+    return null;
 }
 
 export default App;
